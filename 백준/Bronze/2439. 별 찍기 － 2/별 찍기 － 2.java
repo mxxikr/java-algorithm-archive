ee6a7a987deb5a37002f739b91690a76c94e3823
MXXI.kr <mxxikr@gmail.com>
2026-01-24 17:41:48 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 100 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ * ==== 의사 코드 ====
+ */
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            bw.write(" ".repeat(n - i));
+            bw.write("*".repeat(i));
+            bw.newLine();
+        }
+        bw.flush();
+    }
+}
