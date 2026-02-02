ee8cfe9978686d7a574d8b0e8de688d152a2cf91
MXXI.kr <mxxikr@gmail.com>
2026-02-03 07:13:18 +0900

    [Silver II] Title: 스택 수열, Time: 308 ms, Memory: 27044 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -2,69 +2,57 @@ import java.io.*;
 import java.util.Stack;
 
 /**
- * N(수열의 개수) A[](수열 배열)
- * 수열 배열 채우기
- * for(N만큼 반복)
- * {
- * 	if(현재 수열 값 >= 오름차순 자연수) {
- * 		while(값이 같아질 때까지) {
- * 			push()
- * 			(+)저장
- * 		}
- * 	    pop()
- * 	    (-) 저장
- *  } else(현재 수열 값 <  오름차순 자연수) {
- * 		pop()
- * 		if(스택 pop 결과 값 >  수열의 수) NO 출력
- * 		(-) 저장
- *  }
- * }
+ * ==== 의사 코드 ====
+ * N(수열 개수 입력 받기)
+ * stack<Integer> 선언
+ * StringBuilder sb 선언
+ * int count = 1
  *
- * if(NO 값을 출력한 적이 없으면) 저장한 값 출력
+ * for (int i = 0; i < N; i++) {
+ *      int target = 입력 받기
+ *      while (count <= target) {
+ *          stack.push(count);
+ *          sb.append("+\n")
+ *          count++;
+ *      }
+ *      if( stack.peek() == target) {
+ *          stack.pop()
+ *          sb.append("-\n")
+ *      } else {
+ *          System.out.print("NO")
+ *          return;
+ *      }
+ * }
+ * System.out.prinln(sb)
+ *
  */
+
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
+        Stack<Integer> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+
+        int count = 1;
 
         for (int i = 0; i < N; i++) {
-            A[i] =  Integer.parseInt(br.readLine());
-        }
+            int target = Integer.parseInt(br.readLine());
+            while (count <= target) {
+                stack.push(count);
+                sb.append("+\n");
+                count++;
+            }
 
-        StringBuffer buffer = new StringBuffer();
-        Stack<Integer> stack = new Stack<>();
-        int number = 1; // 오름차순 수
-        boolean result = true;
-
-        for (int i = 0; i < A.length; i++) {
-            int su = A[i]; // 현재 수열의 수
-            if (su >= number) { // 현재 수열의 값 >= 오름차순 자연수: 값이 같아질 때까지 push() 수행
-                while (su >= number) { // push()
-                    stack.push(number++);
-                    buffer.append("+\n");
-                }
+            if (stack.peek() == target) {
                 stack.pop();
-                buffer.append("-\n");
-            } else { //현재 수열 값 < 오름차순 자연수: pop()를 수행해 수열 원소를 꺼냄
-                int popNumber = stack.pop();
-                // 스택의 가장 위의 수가 만들어야하는 수열의 수보다 크면 수열을 출력할 수 없음
-                if (popNumber > su) {
-                    System.out.println("NO");
-                    result = false;
-                    break;
-                } else {
-                    buffer.append("-\n");
-                }
+                sb.append("-\n");
+            } else {
+                System.out.println("NO");
+                return;
             }
         }
-        if (result) {
-            bw.write(buffer.toString());
-        }
-
-        bw.flush();
-        bw.close();
-        br.close();
+        System.out.println(sb);
     }
 }
