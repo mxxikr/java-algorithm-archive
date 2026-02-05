31934586b6e0eef431151dece3acb4a50c539258
MXXI.kr <mxxikr@gmail.com>
2026-02-06 08:22:25 +0900

    [Gold IV] Title: 오큰수, Time: 776 ms, Memory: 147936 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,61 +1,64 @@
 import java.io.*;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.nio.Buffer;
+import java.util.*;
 
 /**
- * N(수열개수) A[](수열 배열) ans[](정답 배열)
- * 수열 배열 채우기
- * 최초 스택 초기화 하기
+ * ==== 의사 코드 ====
+ * N(수열 크기 입력받기)
+ * A[N] (숫자 넣을 배열 선언)
+ * ans[N] (정답 배열 선언)
  *
- * for(N만큼 반복) {
- * 	while(스택이 비어있지 않고, 현재 수열의 값이 top에 해당하는 수열보다 클 때까지) {
- * 		pop()
- * 		정답 배열에 오큰수를 현재 수열로 저장하기
- * 	    }
- * 	현재 수열을 스택에 push()
+ * for (i = 0; i < N; i++){
+ *      A[i] = st.token();
  * }
  *
- * while(스택이 빌 때까지) {
- * 	스택에 있는 인덱스의 정답 배열에 -1 저장하기
+ * for (i = 0; i < N; i++){
+ *      ans[i] = -1 // 정답 배열 초기화
  * }
- * 정답 배열 출력하기
+ *
+ * stack<Integer> 선언
+ *
+ * for (i = 0; i < N; i++){
+ *      while (!stack.isEmpty) && A[stack.peek()] < A[i]{ // 스택이 비어 있지 않고 스택의 값보다 배열의 값이 더 클 경우
+ *          int index = stack.pop();
+ *
+ *          ans[index] = A[i] // 정답 배열에 현재 배열의 값을 추가
+ *      } // 스택이 비어있거나 스택의 값보다 배열의 값이 작은 경우
+ *      stack.push(i); // 스택에 현재 배열의 인덱스 push
+ * }
+ *
  */
+
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] inputArray = new int[N]; // 수열 배열 생성
-        int[] answerArray = new int[N]; // 정답 배열 생성
+        int N =  Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        int[] ans = new int[N];
+        Stack<Integer> stack = new Stack<>();
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         for (int i = 0; i < N; i++) {
-            inputArray[i] = Integer.parseInt(st.nextToken());
+            A[i] = Integer.parseInt(st.nextToken());
         }
 
-        Stack<Integer> stack = new Stack<>();
-        stack.push(0); // 처음에는 항상 스택이 비어있으므로 최초 값을 Push해 초기화
+        for (int i = 0; i < N; i++) {
+            ans[i] = -1;
+        }
 
-        for (int i = 1; i < N; i++) {
-            // 스택이 비어있지 않고, 현재 수열이 스택 top 인덱스가 가리키는 수열보다 클 경
-            while (!stack.empty() && inputArray[stack.peek()] < inputArray[i]) {
-                answerArray[stack.pop()] = inputArray[i]; // 정답 배열에 오큰수를 현재 수열로 저장하기
+        for (int i = 0; i < N; i++) {
+            while (!stack.isEmpty() && A[stack.peek()] < A[i]) {
+                int index = stack.pop();
+                ans[index] = A[i];
             }
-            stack.push(i); // 신규 데이터 push
-        }
-        //
-        while (!stack.empty()) {
-            // 반복문을 다 돌고 나왔는데 스택이 비어있지 않다면 빌때까지
-            answerArray[stack.pop()] = -1;
+            stack.push(i);
         }
 
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            bw.write(answerArray[i] + " ");
-            // 출력한다
+            sb.append(ans[i]).append(" ");
         }
-
-        bw.write("\n");
-        bw.flush();
+        System.out.println(sb);
     }
 }
