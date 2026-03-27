6d5da9c71977f4ee262d96fec5140d5d37a10ca3
MXXI.kr <mxxikr@gmail.com>
2026-03-28 05:52:56 +0900

    [Silver III] Title: 수열, Time: 236 ms, Memory: 23828 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.java
+++ 백준/Silver/2559. 수열/수열.java
@@ -1,57 +1,32 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
-/**
- * ==== 의사 코드 ====
- * N K(입력 값 받기)
- * int[] arr[N] 선언
- *  for (int i = 0부터 N까지 ){
- *      arr[i] = st.token
- *  }
- *
- *  int sum = 0
- *  for (int i=0부터 K까지) {
- *      sum += arr[i]
- *  }
- *
- *  int max = sum
- *
- *  for (int K부터 N까지) {
- *      sum += arr[i]
- *      sum -= arr[i - K]
- *      if (sum > max) {
- *          max = sum
- *      }
- *  }
- */
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        int[] arr =  new int[N];
+        int M = Integer.parseInt(st.nextToken());
+        int[] arr = new int[N];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-
-        int sum = 0;
-        for (int i = 0; i < K; i++) {
-            sum += arr[i];
+       
+        int firstSum = 0; // 초기 값 세팅
+        for (int i = 0; i < M; i++) {
+            firstSum += arr[i];
         }
 
-        int max = sum;
+        int max = firstSum; // 최댓값 세팅
 
-        for (int i = K; i < N; i++) {
-            sum += arr[i];
-            sum -= arr[i - K];
-            if (sum > max) {
-                max = sum;
-            }
+        for (int i = M; i < N; i++) {
+            firstSum += arr[i];
+            firstSum -= arr[i - M];
+            max = Math.max(firstSum, max);
         }
-
         System.out.println(max);
     }
 }
