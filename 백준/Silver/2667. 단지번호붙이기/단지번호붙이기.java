803eff56f2576e316760dcfc4c4f90499dbdc0b8
MXXI.kr <mxxikr@gmail.com>
2026-03-30 07:05:13 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,69 +1,14 @@
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.StringTokenizer;
-
-/**
- * ==== 의사 코드 ====
- *  N(지도 크기 입력 받기)
- *  int map[N][N] // 지도 변수 선언
- *  boolean visited[N][N] // 방문 체크 변수 선언
- *
- *  for (int i = 0; i < N; i++) { // 지도 채우기
- *      for (int j = 0; j < N; i++) {
- *          map[i][j] = 입력 받은 값 저장
- *  }
- *
- *  count = 0; // 단지 수
- *  ArrayList<Integer> answer = new ArrayList<>(); // 단지 내 집 개수 담기
- *
- *  for (int i = 0; i < N; i++) { // 지도 훑기
- *      for (int j = 0; j < N; i++) {
- *          if(map[i][j] == 1 이고 visited[i][j] == false이면) {
- *              count++; // 새로운 단지 추가
- *              houseCount = 0; // 이번 단지 집 개수 초기화
- *              dfs(i, j)
- *
- *              answer.add(houseCount);
- *          }
- *      }
-*   }
- *
- *   dfs(int x, int y) {
- *       visited[x][y] = true;
- *       houseCount++;
- *
- *       dx = [-1, 1, 0, 0]
- *       dy = [0, 0, -1, 1]
- *
- *       for (int = 0; i < 4; i++) {
- *           nx = dx[i] + x
- *           ny = dy[i] + y
- *
- *           if (nx, ny가 지도 범위(0보다는 크고 N보다 작은지) 안에 있고) {
- *               if (map[nx][ny] == 1이고 visited[nx][ny] == false이면) {
- *                   dfs(nx, ny);
- *               }
- *           }
- *       }
- *   }
- *
- */
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-    static int[][] map;
-    static boolean[][] visited;
-    static int houseCount;
-    static int N;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-
-        map = new int[N][N];
-        visited = new boolean[N][N];
-
-
+        int N = Integer.parseInt(br.readLine());
+        int[][] map  = new int[N][N];
 
         for (int i = 0; i < N; i++) {
             String line = br.readLine();
@@ -72,45 +17,47 @@ public class Main {
             }
         }
 
-        int count = 0;
-        ArrayList<Integer> answer = new ArrayList<>();
+        boolean[][] visited = new boolean[N][N];
+        ArrayList<Integer> list = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if (map[i][j] == 1 && !visited[i][j]) {
-                    count++;
-                    houseCount = 0;
-                    dfs(i, j);
+                    Queue<int[]> queue = new LinkedList<>();
+                    queue.add(new int[]{i, j});
+                    visited[i][j] = true;
 
-                    answer.add(houseCount);
+                    int count = 1; // 현재 시작하는 집 포함
+                    int[] dx = {0, 0, -1, 1};
+                    int[] dy = {1, -1, 0, 0};
+
+                    while (!queue.isEmpty()) {
+                        int[] current = queue.poll();
+                        int x = current[0];
+                        int y = current[1];
+
+                        for (int k = 0; k < 4; k++) {
+                            int nx = x + dx[k];
+                            int ny = y + dy[k];
+
+                            if (nx >= 0 && ny >= 0 && nx < N && ny < N && !visited[nx][ny] && map[nx][ny] == 1) {
+                                count++;
+                                queue.add(new int[]{nx, ny});
+                                visited[nx][ny] = true;
+                            }
+                        }
+                    }
+                    list.add(count);
                 }
             }
         }
 
-        Collections.sort(answer);
 
-        System.out.println(count);
-        for (int i = 0; i < answer.size(); i++) {
-            System.out.println(answer.get(i));
-        }
-    }
+        Collections.sort(list); // 오름차순 정렬
 
-    private static void dfs(int x, int y) {
-        visited[x][y] = true;
-        houseCount++;
-
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, - 1, 1};
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx >= 0 && nx < N && ny >= 0 && ny < N) {
-                if (map[nx][ny] == 1 && !visited[nx][ny]) {
-                    dfs(nx, ny);
-                }
-            }
+        System.out.println(list.size()); // 총 단지 수
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i)); // 각 단지별 집 수
         }
     }
 }
