21b2bce44c11dbe0c1414c8dc01f6de118c05a12
MXXI.kr <mxxikr@gmail.com>
2026-03-30 06:17:46 +0900

    [Silver II] Title: 유기농 배추, Time: 140 ms, Memory: 15928 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,115 +1,63 @@
 import java.io.*;
-import java.util.*;
-
-/**
- * ==== 의사 코드 ====
- * int[] dx = {0, 0, -1, 1}
- * int[] dy = {-1, 1, 0, 0}
- * int[][] field
- * boolean[][] visited
- *
- * T(테스트 케이스 개수 입력 받기)
- * while (T-- > 0) {
- *     StringTokenizer = br.line()
- *     int M = st.token // 가로 길이
- *     int N  = st.token // 세로 길이
- *     int k = st.token // 배추가 심어져이쓴 위치의 개수
- *
- *     field = new int[M][N]
- *     visited = new boolean[M][N]
- *     int warmCount = 0
- *
- *     for (int i = 0; i < K; i++) {
- *         st = br.line()
- *         int x = st.token()
- *         int y = st.token()
- *
- *         field[x][y] = 1
- *
- *     }
- *
- *     for (int i = 0; i < M; i++){
- *         for (int j = 0; j < N; ㅓ++) {
- *             //배추가 있고 지렁이가 안가봤다면
- *             if (field[i][j] == 1 && !visited[i][j]) {
- *                  warmCount++;    // 지렁이 추가
- *
- *                  dfs(i, j) // dfs 호출
- *             }
- *
- *         }
- *     }
- * }
- *
- * dfs (int x, int y) {
- *    visited[i][j] = true;   // 방문 표시
- *
- *    for (int i = 0;  i <  4; i++) { // 상하좌우 확인
- *        nx = x + dx[i] // 다음 x 좌표
- *        n = y + dy[i]  // 다음 y 좌표
- *
- *        if ( nx >= 0 && nx < M && ny >= 0 && ny < N) { // 밭 범위 벗어났는지 체크
- *            if (field[nx][ny] == 1 && !visited[nx][ny]) { // 배추가 있고 방문 전이라면
- *                  dfs(nx, ny)
- *            }
- *        }
- *    }
- * }
- */
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {-1, 1, 0, 0};
-    static int[][] field;
-    static boolean[][] visited;
-    static int M, N;
-
     public static void main(String[] args) throws IOException {
-        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T =  Integer.parseInt(br.readLine());
 
         while (T-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            M =  Integer.parseInt(st.nextToken());
-            N =  Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
             int K = Integer.parseInt(st.nextToken());
 
-            field = new int[M][N];
-            visited = new boolean[M][N];
-            int warmCount = 0;
+            int[][] map = new int[N][M];
+            boolean[][] visited = new boolean[N][M];
 
             for (int i = 0; i < K; i++) {
                 st = new StringTokenizer(br.readLine());
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
 
-                field[x][y] = 1;
+                map[y][x] = 1;
             }
 
-            for (int i = 0; i < M; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (field[i][j] == 1 && !visited[i][j]) {
-                        warmCount++;
-                        dfs(i, j);
+            int count = 0;
+            int[] dx = {-1, 1, 0, 0};
+            int[] dy = {0, 0, 1, -1};
+
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (map[i][j] == 1 && !visited[i][j]) { // 새로운 배추 덩어리를 발견 시
+                        count++; // 지렁이 추가
+
+                        Queue<int[]> q = new LinkedList<>();
+                        q.add(new int[] {i, j}); // 해당 위치를 시작점으로
+                        visited[i][j] = true;
+
+                        while (!q.isEmpty()) {
+                            int[] current = q.poll();
+                            int y = current[0];
+                            int x = current[1];
+
+                            for (int k = 0; k < 4; k++) {
+                                int nx = x + dx[k];
+                                int ny = y + dy[k];
+
+                                if (nx >= 0 && ny >= 0 && nx < M && ny < N && map[ny][nx] == 1 && !visited[ny][nx]) {
+                                    visited[ny][nx] = true;
+                                    q.add(new int[]{ny, nx});
+                                }
+                            }
+                        }
                     }
                 }
             }
-            System.out.println(warmCount);
-        }
-    }
-
-    private static void dfs(int x, int y) {
-        visited[x][y] = true;
-
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (nx >= 0 && nx < M && ny >= 0 && ny < N) {
-                if (field[nx][ny] == 1 && !visited[nx][ny]) {
-                    dfs(nx, ny);
-                }
-            }
+            System.out.println(count);
         }
     }
 }
